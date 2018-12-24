9f5db98d6be314ce2d0d5198883f4dd50c369795
s1049616166 <39577925+s1049616166@users.noreply.github.com>
2018-12-24 06:36:55 +0000

    Update CT.java

--- CT.java
+++ CT.java
@@ -1,1 +1,13 @@
-1
+--视频
+
+【video】http://www.zzyo.cc【video】
+
+【video2】http://www.9kyy.com【video2】
+
+--音乐
+
+【music】http://host811165811.s484.pppf.com.cn/【music】
+
+--直播
+
+【zhibo】http://ivi.bupt.edu.cn/hls/hunanhd.m3u8【zhibo】
